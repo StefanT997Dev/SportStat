0ffc8c16be13b4511c03afd83191b314332dd005
Stefan <stefantosic.dev@gmail.com>
2021-03-16 16:38:21 +0100

    Created the UI for player entry

---
+++ app/src/main/java/com/nistruct/sportstat/EnterPlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.nistruct.sportstat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EnterPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_player)
+    }
+}
