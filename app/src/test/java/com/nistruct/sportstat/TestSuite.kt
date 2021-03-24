33238915b9484e9e557ca70c920eca4b8766bd74
imbactbulletz <stefan@nistruct.com>
2021-03-24 11:06:10 +0100

    Add root test suite

---
+++ app/src/test/java/com/nistruct/sportstat/TestSuite.kt
@@ -0,0 +1,11 @@
+package com.nistruct.sportstat
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * Root test suite which consists of Unit, Integration and UI suites.
+ */
+@RunWith(Suite::class)
+class TestSuite {
+}
