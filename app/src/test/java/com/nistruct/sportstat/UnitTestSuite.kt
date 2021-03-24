352157a9928852c966167b55d6250ffceb6fd4ee
imbactbulletz <stefan@nistruct.com>
2021-03-24 11:10:21 +0100

    Add ViewModel unit test suite

--- app/src/test/java/com/nistruct/sportstat/UnitTestSuite.kt
+++ app/src/test/java/com/nistruct/sportstat/UnitTestSuite.kt
@@ -1,8 +1,12 @@
 package com.nistruct.sportstat
 
+import com.nistruct.sportstat.viewmodel.ViewModelUnitTestSuite
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
+@Suite.SuiteClasses(
+    ViewModelUnitTestSuite::class
+)
 class UnitTestSuite {
 }
---
+++ app/src/test/java/com/nistruct/sportstat/viewmodel/ViewModelUnitTestSuite.kt
@@ -0,0 +1,8 @@
+package com.nistruct.sportstat.viewmodel
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+class ViewModelUnitTestSuite {
+}
