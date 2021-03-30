da19c6eb3b1c604315669c517f8e71dc62109180
Stefan <stefantosic.dev@gmail.com>
2021-03-30 11:17:24 +0200

    Added functionality for camera picking players

--- app/src/main/java/com/nistruct/sportstat/ui/enter_player/EnterPlayerActivity.kt
+++ app/src/main/java/com/nistruct/sportstat/ui/enter_player/EnterPlayerActivity.kt
@@ -1,22 +1,33 @@
 package com.nistruct.sportstat.ui.enter_player
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.nistruct.sportstat.R
-import com.nistruct.sportstat.repository.PlayerRepositoryImpl
-import com.nistruct.sportstat.ui.team.EnterTeamViewModel
-import com.nistruct.sportstat.usecase.PostPlayerUseCaseImpl
 import com.nistruct.sportstat.usecase.result.DataResult
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_enter_player.*
-import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class EnterPlayerActivity: AppCompatActivity() {
-    @Inject lateinit var enterPlayerViewModelFactory:EnterPlayerViewModelFactory
+class EnterPlayerActivity : AppCompatActivity() {
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
+
+    @Inject
+    lateinit var enterPlayerViewModelFactory: EnterPlayerViewModelFactory
 
     private lateinit var viewModel: EnterPlayerViewModel
 
@@ -24,7 +35,10 @@ class EnterPlayerActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_player)
 
-        viewModel = ViewModelProvider(this, enterPlayerViewModelFactory).get(EnterPlayerViewModel::class.java)
+        viewModel = ViewModelProvider(
+            this,
+            enterPlayerViewModelFactory
+        ).get(EnterPlayerViewModel::class.java)
 
         savePlayerButton.setOnClickListener {
             setPlayer()
@@ -44,6 +58,23 @@ class EnterPlayerActivity: AppCompatActivity() {
                 }
             }
         }
+
+        addImageTextView.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
+            }
+        }
     }
 
     private fun setPlayer() {
@@ -52,5 +83,37 @@ class EnterPlayerActivity: AppCompatActivity() {
         viewModel.playerImage = playerImageView.toString()
         viewModel.playerPhoneNumber = playerPhoneNumberEditText.text.toString()
         viewModel.playerPosition = roleOfPlayerSpinner.selectedItem.toString()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == CAMERA_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                Toast.makeText(
+                    this,
+                    "Oops you denied the permission for camera.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode== CAMERA_REQUEST_CODE){
+                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap
+                playerImageView.setImageBitmap(thumbnail)
+            }
+        }
     }
 }
